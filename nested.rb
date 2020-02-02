244b1ef470cb1da432990a0b174b29a73977235c
Kwane Smith <smithkwane6@gmail.com>
2020-02-02 22:43:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,6 +67,10 @@ def adding_matz
 # }
 
 	programmer_hash =
+	programmer_hash[:yukihiro_matsumoto] = {
+	:known_for => "Ruby",
+	:languages => ["LISP, C"]
+}
  		{
         :grace_hopper => {
           :known_for => "COBOL",
